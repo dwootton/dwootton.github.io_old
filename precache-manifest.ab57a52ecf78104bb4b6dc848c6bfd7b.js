168ab7edd817fc2c120906f2f12378256e1c45b5
Dylan Wootton <wootton.dylan@gmail.com>
2020-09-23 18:50:11 -0700

    Updates

---
+++ precache-manifest.ab57a52ecf78104bb4b6dc848c6bfd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090fcb7d6dc8c36744725faedba8e42b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc06b48e2ee334a8c7d",
+    "url": "/static/css/main.22878451.chunk.css"
+  },
+  {
+    "revision": "3f386278f6de3de87cce",
+    "url": "/static/js/2.57a5cd48.chunk.js"
+  },
+  {
+    "revision": "aef66ac16c4bc247229ecc2dd8a97c66",
+    "url": "/static/js/2.57a5cd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc06b48e2ee334a8c7d",
+    "url": "/static/js/main.bbafef96.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "33fadb9a71141d5b6ee6d92040dc33e4",
+    "url": "/static/media/Logo.33fadb9a.svg"
+  }
+]);
