ce310839de55caaa7b6311a84b65a1e21b89f5ad
Dylan Wootton <wootton.dylan@gmail.com>
2020-12-15 08:37:37 -0800

    external project links

--- src/common/Components/Projects/ProjectPage.jsx
+++ src/common/Components/Projects/ProjectPage.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styles from "./projectStyles.module.scss";
 import { useSpring, animated } from "react-spring";
 import { Fade } from "react-reveal";
+
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo'; // demo video
+import WebIcon from '@material-ui/icons/Web'; // live url
+import DescriptionIcon from '@material-ui/icons/Description'; // paper 
+
+
 const ProjectPage = ({ projectConfiguration }) => {
   return (
     <div>
@@ -15,7 +21,7 @@ const ProjectPage = ({ projectConfiguration }) => {
   );
 };
 
-const TitlePhoto = ({ image, title, titleQuestion, fullDescription,imageGallery }) => {
+const TitlePhoto = ({ image, title, titleQuestion, fullDescription,imageGallery, liveURL, githubURL, videoURL, }) => {
   return (
     <div>
       <div className={styles.titlePhoto} style={{ backgroundImage: image }}>
@@ -28,6 +34,7 @@ const TitlePhoto = ({ image, title, titleQuestion, fullDescription,imageGallery 
       </div>
       <div className={styles.content}>
       <Fade><h5 className={styles.fullDescription}>{fullDescription}</h5></Fade>
+      <Fade delay={150}><ExternalLinks liveURL={liveURL} githubURL={githubURL} videoURL={videoURL} ></ExternalLinks></Fade>
         {imageGallery && imageGallery.map(image=>{
             return <Fade><img className={styles.galleryImage} alt={image.description} src={image.image}></img> </Fade>
         })}
@@ -35,6 +42,11 @@ const TitlePhoto = ({ image, title, titleQuestion, fullDescription,imageGallery 
     </div>
   );
 };
+
+const ExternalLinks = ({liveURL,githubURL,videoURL})=>{
+    return <div className={styles.externalLinks}>{liveURL && <a href={liveURL} rel="noopener noreferrer nofollow">Software</a>}{githubURL && <a href={githubURL} rel="noopener noreferrer nofollow">Code</a>}{videoURL && <a href={videoURL} rel="noopener noreferrer nofollow">Demo</a>}</div>
+}
+
 export default ProjectPage;
 
 export const SidePhotoFade = ({ children, delay = 0, ...props }) => {
--- src/common/Configurations/projects.js
+++ src/common/Configurations/projects.js
@@ -23,10 +23,9 @@ let projects = [
   {
     id: "reVISit",
     title: "reVISit",
-    image:"https://i.imgur.com/XdteUVq.png",
+    image: "https://i.imgur.com/R9kGn0y.png",
     shortName: "RV",
     year: 2020,
-    
 
     liveURL: "https://revisit-analytics.github.io/reVISit/",
     githubURL: "https://github.com/visdesignlab/reVISit",
@@ -40,36 +39,72 @@ let projects = [
       SKILLS_ENUMS["dm"],
       SKILLS_ENUMS["interviews"],
     ],
-    titleQuestion:"How can we better design interactions for visualizations?",
+    titleQuestion: "How can we better design interactions for visualizations?",
     videoURL: "",
     featured: true,
     shortDescription:
       "reVISit is a visualization tool that enables the exploration of user interaction data.",
     fullDescription:
       "Understanding user interactions with websites is hard. Often times, UX researchers are tasked with parsing through gigabytes of telemetry logs in order to understand what it is that users are doing. reVISit is an open-source visualization tool that enables users to quickly see users interactions and how those correlate with other tracked metrics. ",
-    imageGallery:[{description:"Home interface of reVISit.",image:"https://i.imgur.com/l4zUeF6.png"},{description:"reVISit workstream integration.",image:"https://i.imgur.com/XTPQW4o.png"}, {description:"Qualitative Coding in the reVISit interface.",image:"https://i.imgur.com/exavJfD.png"}]
+    imageGallery: [
+      { description: "Home interface of reVISit.", image: "https://i.imgur.com/l4zUeF6.png" },
+      { description: "reVISit workstream integration.", image: "https://i.imgur.com/XTPQW4o.png" },
+      {
+        description: "Qualitative Coding in the reVISit interface.",
+        image: "https://i.imgur.com/exavJfD.png",
       },
+    ],
+  },
   {
     id: "arcticExplorer",
     title: "Arctic Explorer",
     shortName: "AE",
     year: 2019,
-    image:"https://i.imgur.com/9gGUAt5.png",
+    image: "https://i.imgur.com/9gGUAt5.png",
     liveURL: "http://dylanwootton.com/Arctic-Explorer",
     githubURL: "https://github.com/dwootton",
     projectTags: [PROJECT_TAG_ENUM["viz"], PROJECT_TAG_ENUM["fs"]],
     skills: [SKILLS_ENUMS["d3"], SKILLS_ENUMS["js"], SKILLS_ENUMS["interviews"]],
     featured: true,
-    titleQuestion:"In a rapidly changing environment, how can we understand trends of sea-ice density?",
+    titleQuestion:
+      "In a rapidly changing environment, how can we understand trends of sea-ice density?",
     shortDescription:
       "Arctic Explorer enables researchers to analyze sea-ice concentration data for the Arctic.",
     fullDescription:
-      "Arctic Explorer enables Arctic researchers to study how Arctic Ice concentration changes over time. ",
+      "The Arctic seascape plays an important role in North American trade and defense; however, over recent years, the Arctic has undergone radical changes, altering current shipping routes and opening up new previously blocked paths. This work presents Arctic Explorer, a tool to visualize sea-ice concentration along paths over time. Arctic Explorer could be used to circumnavigate areas expected to be blocked with ice and discover new routes that may have opened up. ",
+    imageGallery: [
+      {
+        description: "Mockup image of the Arctic Explorer Interface",
+        image: "https://i.imgur.com/BXEx4bd.png",
+      },
+      {
+        description: "The Arctic Explorer User Interface",
+        image: "https://i.imgur.com/YJj0HDy.png",
+      },
+      { description: "Guide for how to read a TOSM", image: "https://i.imgur.com/4Lixmqf.png" },
+      {
+        description: "Interaction across northwest passage",
+        image: "https://i.imgur.com/s96oiXg.gif",
+      },
+      {
+        description: "Whiteboard prototypes of Arctic Explorer.",
+        image: "https://i.imgur.com/MiY0wQ7.png",
+      },
+      {
+        description: "Whiteboard prototype of interactions.",
+        image: "https://i.imgur.com/tgI4smh.png",
+      },
+      {
+        description: "Beautiful Temporal Ordered Spatial Matrix for the Northwest Passage",
+        image: "https://i.imgur.com/VzhnoC1.png",
+      },
+    ],
   },
   {
     id: "aqExplorer",
-    title: "AQ Explorer",
+    title: "AirU",
     shortName: "AQ",
+    image: "",
     year: 2018,
     liveURL: "http://dylanwootton.com/AQExplorer",
     githubURL: "https://github.com/dwootton/AQExplorer",
